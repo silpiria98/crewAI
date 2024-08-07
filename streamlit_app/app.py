fec13709b79566f31a0594e9f77dda2eb86e690f
노하일 <rohhail@nohail-ui-MacBookPro.local>
2024-08-07 16:43:05 +0900

    add streamlit app

---
+++ streamlit_app/app.py
@@ -0,0 +1,215 @@
+import streamlit as st
+import story_generator
+import re
+
+st.set_page_config(
+    page_title="아이펠 던전 AI"
+)
+
+if "page" not in st.session_state:  # 초기 상태 설정
+    st.session_state.page = "page1"
+
+if st.session_state.page == "page1":  #첫 화면
+    with st.container():
+        # 타이틀
+        st.title("AIFFEL 던전 AI")
+
+        st.image("images/dragon.jpg", caption="", use_column_width=True)
+        # 게임 설명
+        st.subheader( "선택의 기로에서 마주하는 수많은 갈림길 \n 당신의 결정이 이야기의 향방을 바꿉니다. 신중하게 선택하고, 그 결과를 마주하세요.")
+
+        st.write("")
+
+        if st.button("GAME START!", key="gamestart"):
+            st.session_state.page = "page2"
+            st.rerun()
+        
+elif st.session_state.page == "page2":  # 컨테이너 숨김 상태일 때
+    # 컨테이너가 사라진 후에 표시할 내용 (예: 게임 화면)
+    st.subheader("원하시는 모험의 테마를 입력해주세요!")
+    col1, col2 = st.columns([1,1])
+
+    if "theme" not in st.session_state:
+        st.session_state.theme = ""  # 초기값 설정
+
+    with col1:
+        theme = st.text_input("",placeholder="예: 어둠 속의 미궁, 고대 유적 탐험")
+    
+    if st.button("NEXT",key="nextfrom2page"):
+        if theme.strip() == "":  # 입력값이 비어있거나 공백만 있는 경우
+            st.warning("테마를 입력해주세요!")  # 경고 메시지 표시
+        else:
+            st.session_state.page = "page3"
+            st.session_state.theme = theme #테마 입력값 유지
+            st.rerun()
+        
+elif st.session_state.page == "page3":
+    st.title("캐릭터 소개")
+    st.caption(f"선택된 테마 : {st.session_state.theme}")
+
+
+    #플레이어 정보 state
+    if "player_information" not in st.session_state:  
+        st.session_state.player_information = ""
+    
+    if "player_info_count" not in st.session_state:  
+        st.session_state.player_info_count = 0
+
+    if st.session_state.player_info_count == 0:
+        player_info = story_generator.character_creation(story_generator.story_crew,
+                                                        st.session_state.theme,
+                                                        "story.md",
+                                                        "log.md",)
+        st.session_state.player_info_count += 1
+        st.session_state.player_information = player_info
+    
+        st.write(st.session_state.player_information)
+
+    def movetopage4():
+        st.session_state.page = "page4"
+    st.button("NEXT",key="pagefrom3page",on_click=movetopage4)
+                
+
+elif st.session_state.page == "page4":
+    if "show_story" not in st.session_state:
+        st.session_state.show_story = True
+
+    if st.session_state.show_story:   #테마가 rerun 되었을때
+        print(st.session_state['show_story'])   #############################################################################################    
+        st.caption(f"선택된 테마 : {st.session_state.theme}")
+        
+        # 상황 count
+        if "count_scene" not in st.session_state:
+            st.session_state.count_scene = 1
+        
+        if st.session_state.count_scene == 8: #7회까지만 진행 후 엔딩
+            st.session_state.page = "page5"
+            st.session_state.show_story = False
+            st.rerun()  
+        
+        st.text(f"#상황. {st.session_state.count_scene}")
+        
+            
+        # 생성된 글 저장 
+        if "created_story" not in st.session_state:  
+            st.session_state.created_story = ""
+        
+        created_story = story_generator.story_creation(st.session_state.count_scene,
+                                                       story_generator.story_crew, 
+                                                        st.session_state.theme, 
+                                                        "story.md",
+                                                        "log.md")
+        
+        st.session_state.count_scene += 1
+
+        st.session_state.created_story = created_story
+
+        st.write(created_story) #스토리 화면
+        
+        st.text("")
+    
+        # 선택지 생성
+        choices_creation = story_generator.create_options(story_generator.story_crew, 
+                                                            st.session_state.theme, 
+                                                            "story.md",
+                                                            "log.md")
+        #선택된 선택지 선택
+        if "option_selected" not in st.session_state:  
+            st.session_state.option_selected = ""
+        if "option_selected_num" not in st.session_state:  
+            st.session_state.option_selected_num = 0
+
+        choices_lines = choices_creation.split("\n")
+
+        choices_lines = [sentence for sentence in choices_lines if re.match(r"^[1-5]\.", sentence)] #선택지 배열 
+            
+            
+        #선택지 1
+        if st.button(choices_lines[0], key=0):
+            st.session_state.option_selected = choices_lines[0]
+            st.session_state.option_selected_num = 0
+            continue_story = story_generator.user_selection_streamlit(st.session_state.option_selected,
+                                                                st.session_state.option_selected_num, 
+                                                                "story.md", 
+                                                                "log.md") #사용자가 선택한 정보를 기록
+            st.rerun()
+
+        #선택지 2
+        elif st.button(choices_lines[1],key=1):
+            st.session_state.option_selected = choices_lines[1]
+            st.session_state.option_selected_num = 1
+            continue_story = story_generator.user_selection_streamlit(st.session_state.option_selected,
+                                                                st.session_state.option_selected_num, 
+                                                                "story.md", 
+                                                                "log.md")
+            st.rerun()
+        
+        #선택지 3
+        elif st.button(choices_lines[2],key=2):
+            st.session_state.option_selected = choices_lines[2]
+            st.session_state.option_selected_num = 2
+            continue_story = story_generator.user_selection_streamlit(st.session_state.option_selected,
+                                                                st.session_state.option_selected_num, 
+                                                                "story.md", 
+                                                                "log.md")
+            st.rerun()
+
+        #선택지 4
+        elif st.button(choices_lines[3],key=3):
+            st.session_state.option_selected = choices_lines[3]
+            st.session_state.option_selected_num = 3
+            continue_story = story_generator.user_selection_streamlit(st.session_state.option_selected,
+                                                                st.session_state.option_selected_num, 
+                                                                "story.md", 
+                                                                "log.md")
+            st.rerun()
+    
+        col1, col2, col3 = st.columns([1,1,1])
+        with col1:
+            st.button("직접 선택지 작성",key=4)
+
+        #선택지 5
+        with col3:
+            def nextpage_clicked_onpage4():
+                st.session_state.page = "page5"
+                st.session_state.show_story = False
+            
+            st.button("엔딩 보기",key=5,on_click=nextpage_clicked_onpage4)
+                
+
+elif st.session_state.page == "page5":
+    
+    
+    if "show_ending" not in st.session_state:  
+            st.session_state.show_ending = True
+    if st.session_state.show_ending:
+        print(st.session_state['show_ending'])   #############################################################################################
+        print(st.session_state['show_story'])
+        st.caption(f"선택된 테마 : {st.session_state.theme}")
+        st.text(f"#엔딩..")
+        
+        ending_story = story_generator.story_ending(story_generator.story_crew, 
+                                                    st.session_state.theme, 
+                                                    "story.md",
+                                                    "log.md")
+        st.write(ending_story)
+        #사용자 선택 / 선택한 내용 저장
+        def nextpage_clicked_onpage5():
+            st.session_state.page = "page6"
+            st.session_state.show_ending = False
+        
+        st.button("NEXT",key="pagefrom6page",on_click= nextpage_clicked_onpage5)
+                    
+        
+            
+
+elif st.session_state.page == "page6":
+    st.title("당신의 모험 기록")
+    st.caption(f"선택된 테마 : {st.session_state.theme}")
+    st.text("")
+
+    with open("story.md", "r", encoding="utf-8") as f:
+      log_content = f.read()
+    
+    # Streamlit에 출력
+    st.markdown(log_content, unsafe_allow_html=True)
---
+++ streamlit_app/story_generator.py
@@ -0,0 +1,284 @@
+
+
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+# Warning control
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import tools
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from openai import OpenAI
+import os, requests
+
+llm1 = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash", google_api_key=os.getenv("GEMINI_API_KEY")
+)
+llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=os.getenv("GPT_API_KEY"))
+
+def generateimage(chapter_content):
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=f"이미지의 내용: {chapter_content}. Style: Illustration. Create an illustration incorporating a vivid palette with an emphasis on shades of azure and emerald, augmented by splashes of gold for contrast and visual interest. The style should evoke the intricate detail and whimsy of early 20th-century storybook illustrations, blending realism with fantastical elements to create a sense of wonder and enchantment. The composition should be rich in texture, with a soft, luminous lighting that enhances the magical atmosphere. Attention to the interplay of light and shadow will add depth and dimensionality, inviting the viewer to delve into the scene. DON'T include ANY text in this image. DON'T include colour palettes in this image.",
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+    filename = f"image_{chapter_content[:10].replace(' ', '_')}.png"
+    filepath = os.path.join(os.path.dirname(os.getcwd()), "img", filename)
+
+    image_response = requests.get(image_url)
+    if image_response.status_code == 200:
+        with open(filepath, "wb") as file:
+            file.write(image_response.content)
+    else:
+        print("Failed to download the image.")
+        return ""
+
+    return filepath
+
+story_agent = Agent(
+    role="이야기 생성자",
+    goal="{theme} 배경으로 흥미로운 상황을 생성합니다.",
+    backstory="당신은 유명한 작가입니다."
+    "사용자의 요구에 맞게 자연스럽게 흥미로운 이야기를 만들어 낼 수 있습니다."
+    "현제 롤플레잉을 하고 있습니다, 이야기를 만들어 나갈 때 주인공의 행동을 당신이 결정해서는 안됩니다. ",
+    memory=True,
+    allow_delegation=False,
+    llm=llm1,
+)
+
+# 선택지 에이전트: 상황에 맞는 4가지 선택지를 제공
+# 추가로 종료 선택지와, 자유롭게 입력하는 것도 허용
+choice_agent = Agent(
+    role="선택지 제공자",
+    goal="주어진 상황을 보고 주인공이 행동할 선택지를 생성합니다.",
+    backstory="당신은 상황에 맞게 선택지를 제시하는 역할을 맡고 있습니다."
+    "이야기의 주인공이 당신의 선택지 중 하나를 선택할 것입니다. "
+    "선택지를 통해 다른 이야기가 전개될 수 있도록 선택지를 생성하세요. ",
+    memory=True,
+    allow_delegation=False,
+    llm=llm1,
+)
+
+# 결말 에이전트
+ending_agent = Agent(
+    role="이야기 결말 생성자",
+    goal="주어진 이야기를 보고 결말을 내립니다.",
+    backstory=" 많은 사람이 납득가능하게 이야기를 마무리 할 수 있습니다. ",
+    memory=True,
+    allow_delegation=False,
+    llm=llm1,
+)
+
+# 주인공 생성 에이전트
+create_player_agent = Agent(
+    role="이야기의 주인공 생성자",
+    goal="{theme} 배경에 어울리는 인물 한 명을 생성합니다. ",
+    backstory="사람에 대해 자새하게 묘사할 수 있습니다.",
+    memory=True,
+    allow_delegation=False,
+    llm=llm1,
+)
+
+# 이야기 생성 작업
+story_creation_task = Task(
+    description="{theme} 배경으로 흥미로운 상황을 생성하세요."
+    "주인공의 선택을 마음대로 결정해서는 안됩니다. "
+    "주인공이 처한 상황, 다른 인물이 걸어오는 대사 등을 생성합니다. "
+    "이야기는 주인공이 어떤 선택을 해야 할 상황에서 끝나도록 하세요."
+    "이전의 내용이 있다면 이어지도록 상황을 생성하세요"
+    "{previous_story}에 이어지는 새로운 상황을 생성하세요",
+    expected_output="200글자 내외의 흥미로운 상황이 생성됩니다."
+    "주인공의 어떤 행동을 결심하거나 행동하거나, 말하거나 하는 내용을 생성하면 안됩니다.  "
+    "주인공이 어떤 행동을 할지 궁금해하지 말고 스토리만 생성합니다. ",
+    agent=story_agent,
+)
+
+# 선택지 생성 작업
+# 필요하면 추가 : "선택지는 1.~ 2.~ 3.~ 4.~ 순으로 제공되고 선택지마다 줄바꿈을 해서 출력하세요"
+choice_creation_task = Task(
+    description="이야기 상황에 맞는 4가지 선택지를 생성하세요. 각 선택지는 이야기와 관련이 있어야 합니다."
+    "{previous_story}와 이어지며, 마지막으로 제시된 상황에 맞는 선택지를 생성하세요. \n"
+    "5번째 선택지는 고정적으로 '여기서 이야기의 마무리합니다.'입니다.",
+    expected_output="상황에 맞는 4가지 선택지가 생성됩니다.\n"
+    "모든 선택지 앞에 1. 2. 3. 4. 같이 숫자를 붙이세요\n"
+    "각 선텍지는 줄바꿈 하여 출력하세요",
+    agent=choice_agent,
+)
+
+
+# 이야기 결말 작업
+ending_task = Task(
+    description="{previous_story}를 보고 적절한 결말을 생성하세요",
+    expected_output="줄글의 형식으로 생성하세요." "반드시 한국어로 생성하세요.",
+    agent=ending_agent,
+)
+
+# 주인공 생성 작업
+create_player_task = Task(
+    description="{theme} 배경에 어울리는 인물 한명을 생성합니다. "
+    "우선 그 인물의 직업을 5개정도 생성하고 무작위로 하나를 선택합니다. "
+    "그 인물의 인적사항을 출력합니다",
+    expected_output="당신은 '(직업)'입니다'로 시작해서 인적사항을 적습니다.",
+    agent=ending_agent,
+)
+
+story_crew = Crew(
+    agents=[
+        story_agent,
+        choice_agent,
+        ending_agent,
+        create_player_agent,
+        # summerize_agent
+    ],
+    tasks=[
+        story_creation_task,
+        choice_creation_task,
+        ending_task,
+        create_player_task,
+        # story_summerization_task
+    ],
+    process=Process.sequential,
+    verbose=True,
+)
+
+
+
+        
+
+#캐릭터 생성함수
+def character_creation(crew,theme,storyfile,logfile):
+        # 플레이어 생성
+    crew.tasks = [create_player_task]
+    player_info = crew.kickoff(inputs={"theme": theme, "previous_story": ""})
+
+    # 플레이어 정보가 든 파일 생성
+    # 스토리만
+    with open(storyfile, "w", encoding="utf-8") as file:
+        file.write(f"{player_info}\n\n\n")
+    # 스토리와 선택지 포함해서 모든 정보 저장
+    with open(logfile, "w", encoding="utf-8") as file:
+        file.write(f"{player_info}\n\n\n")
+    
+    return player_info
+
+# 이야기 생성 함수
+def story_creation(chapter_num,crew, theme, storyfile, logfile):
+
+    # 스토리 파일 읽고 이야기 생성
+    with open(storyfile, "r", encoding="utf-8") as file:
+        previous_story = file.read()
+
+    crew.tasks = [story_creation_task]
+    story_result = crew.kickoff(
+        inputs={"theme": theme, "previous_story": previous_story}
+    )
+    story_text = f"### Chapter{chapter_num} :\n{story_result}\n\n"
+
+    # 초기 캐릭터 만들때 파일을 생성해서 그냥 a로
+    with open(logfile, "a", encoding="utf-8") as file:
+        file.write(story_text)
+    with open(storyfile, "a", encoding="utf-8") as file:
+        file.write(story_text)
+    return story_result
+
+
+# 선택지 생성 함수
+def create_options(crew, theme, storyfile, logfile):
+
+    # 요약 파일 및 방금 생성된 이야기
+
+    with open(storyfile, "r", encoding="utf") as file:
+        previous_story = file.read()
+
+    crew.tasks = [choice_creation_task]
+    choices_creation = crew.kickoff(
+        inputs={"theme": theme, "previous_story": previous_story}
+    )
+    story_text = f"{choices_creation}\n\n\n"
+
+    # 로그에 저장
+    with open(logfile, "a", encoding="utf-8") as file:
+        file.write(story_text)
+
+    return choices_creation
+
+
+# 스트림릿 용 사용자 선택 함수
+def user_selection_streamlit(choices_creation, selected_num,storyfile, logfile):
+    
+    story_text = f"### 사용자 선택 : {selected_num}\n\n\n"
+
+
+    line = f"{choices_creation}\n\n\n"
+    
+    # 로그에 사용자가 선택한 숫자 추가
+    with open(logfile, "a", encoding="utf-8") as file:
+        file.write(story_text)
+
+    # story.md 에 선택한 내용 추가
+    with open(storyfile, "a", encoding="utf-8") as file:
+        file.write(line)
+
+
+    return True
+
+def story_ending(crew, theme, storyfile, logfile):
+            
+            with open(storyfile, "r", encoding="utf-8") as file:
+                previous_story = file.read()
+
+            # 이야기 생성
+            crew.tasks = [ending_task]
+
+            story_result = crew.kickoff(
+                inputs={"theme": theme, "previous_story": previous_story}
+            )
+            story_result_md = f"### 엔딩 :\n{story_result}\n\n"
+
+            # 이야기 내용을 파일에 저장
+            with open(storyfile, "a", encoding="utf-8") as file:
+                file.write(story_result_md)
+
+            # 로그 기록
+            with open(logfile, "a", encoding="utf-8") as file:
+                file.write(story_result_md)
+
+            return story_result
+    
+
+# def execute_story(
+#     crew,
+#     theme,
+#     logfile="log.md",
+#     storyfile="story.md",
+#     # summary = 'summary.md',
+#     # generated_story = 'generated_story.md'
+# ):
+#     character_creation(crew,theme,storyfile,logfile)
+
+            
+#     for i in range(9):        
+
+#         # 요약을 읽기 / 이야기 생성 / 문장 저장
+#         story_result = story_creation(crew, theme, storyfile, logfile)
+
+#         # 이미지 생성 부분 result가 파일 위치
+#         # result = generateimage(story_result)
+        
+#         # 선택지 생성 / 선택지 내용 저장
+#         choices_creation = create_options(crew, theme, storyfile, logfile)
+
+#         # 사용자 선택 / 선택한 내용 저장
+#         continue_story = user_selection(choices_creation, storyfile, logfile)
